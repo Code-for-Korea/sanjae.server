2c1aafdcd07c7a6197dea480d8d781ae15574d06
achim koh <3594463+achimkoh@users.noreply.github.com>
2021-05-26 21:55:35 +0900

    #7 상당인과관계를 db table에 추가

--- case_app/models.py
+++ case_app/models.py
@@ -17,6 +17,7 @@ class Ruling(models.Model):
     그 밖의 항목
     related_case: 판결문에서 추출한 연관판결. f'{법원},{사건번호},{n심}-{법원},{사건번호},{n심}...' 형식의 문자열
     working_condition: 업무환경 (수기 입력)
+    causality: 상당인과관계 (수기 입력)
     disease: 표준 질병코드 + 질병명 (KCD 8차 질병코드 옵션 중 선택)
     '''
     case_number = models.CharField(max_length=255, help_text='사건번호') # 보통 13자 이내
@@ -30,10 +31,10 @@ class Ruling(models.Model):
     # 연관사건이 ForeignKey가 될 수 있을까? 현재 목록에 없는 사건번호도 있어서 일괄적용은 어려울 듯.
     # 하지만 db에 존재하는 사건일 경우 조회할 수 있으면 좋겠다
     working_condition = models.TextField(blank=True, help_text='업무환경')
+    causality = models.TextField(blank=True, help_text='상당인과관계')
     disease = models.CharField(max_length=255, blank=True, default='', help_text='질병분류')
 
     # TODO 사건연도 분리
-    # TODO 법원 이름 띄어쓰기 다른 케이스 존재. 통일
     # TODO 법원이 별도 테이블로 존재해야 하는가?
 
     def __str__(self):
