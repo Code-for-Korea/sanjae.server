ac334a29393d154194f5585f7d7f044cc8aff1b6
Wonil <red112@naver.com>
2021-05-27 21:32:55 +0900

    질병코드 팝업에서 선택하기 적용

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
         template_name='admin/login.html',
         ), {'next': '/'}),
     path('logout/', auth_views.LogoutView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
