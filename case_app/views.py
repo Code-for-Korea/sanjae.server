33b53fbc82158e0dea9fb3ea6725bfdd23d13d07
Restyled.io <commits@restyled.io>
2021-05-28 02:22:11 +0000

    Restyled by reorder-python-imports

--- case_app/views.py
+++ case_app/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse
 
 from .models import Ruling
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 
 from case_app.views import redirect_to_rulings
 
